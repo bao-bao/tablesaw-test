ec234d72519c37ede84deea81f7efdd941e4eea4
包天宇 <1452765@tongji.edu.cn>
2017-05-30 18:05:57 +0800

    PackedLocalDate test demo

--- src/unittest/columns/packeddata/TestPackedLocalDate.java
+++ src/unittest/columns/packeddata/TestPackedLocalDate.java
@@ -1,6 +1,20 @@
 package columns.packeddata;
 
+import com.github.lwhite1.tablesaw.columns.packeddata.PackedLocalDate;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 /* Created by AMXPC on 2017/5/28. */
 
 public class TestPackedLocalDate {
+
+    @Test
+    public void testToEpochDay() {
+        long expected = -865389;
+        int input = 1895696408;
+        long result = PackedLocalDate.toEpochDay(input);
+        assertThat(result, is(expected));
+    }
+
 }
