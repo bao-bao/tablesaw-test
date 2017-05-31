9501d7830134f65f99f477c419c80cc6545f427c
包天宇 <1452765@tongji.edu.cn>
2017-05-31 11:15:11 +0800

    TypeUtils test demo

--- src/unittest/io/TestTypeUtils.java
+++ src/unittest/io/TestTypeUtils.java
@@ -1,6 +1,31 @@
 package io;
 
+import com.github.lwhite1.tablesaw.api.ColumnType;
+import com.github.lwhite1.tablesaw.api.FloatColumn;
+import com.github.lwhite1.tablesaw.columns.Column;
+import com.github.lwhite1.tablesaw.io.TypeUtils;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 /* Created by AMXPC on 2017/5/28. */
 
 public class TestTypeUtils {
+
+    @Test
+    public void testNewColumn() {
+        String errorMessage = "";
+        Column result = null;
+        try {
+            result = TypeUtils.newColumn("canvas", ColumnType.valueOf(""));
+        }
+        catch (IllegalArgumentException e) {
+            errorMessage = e.getMessage();
+        }
+        assertThat(result, is(nullValue()));
+        if(result != null) {
+            assertThat(result.name(), is("canvas"));
+        }
+        assertThat(errorMessage, is(""));
+    }
 }
