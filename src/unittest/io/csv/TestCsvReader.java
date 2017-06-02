71f4e35b7bc3776d31e538ead1dba89d53635c85
包天宇 <1452765@tongji.edu.cn>
2017-06-02 19:13:29 +0800

    CsvReader test part

--- src/unittest/io/csv/TestCsvReader.java
+++ src/unittest/io/csv/TestCsvReader.java
@@ -13,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
@@ -20,6 +21,7 @@ import static org.hamcrest.Matchers.*;
 public class TestCsvReader {
     String fileName;
     ColumnType[] types;
+    boolean header;
 
     @Before
     public void setUp() {
@@ -35,13 +37,12 @@ public class TestCsvReader {
                 ColumnType.LOCAL_DATE_TIME,
                 ColumnType.CATEGORY
         };
-
+        header = true;
     }
 
     @Test
     public void testRead() throws IOException {
         InputStream stream = new FileInputStream(fileName);
-        boolean header = false;
         Table table = CsvReader.read(fileName, types, header, ',', stream);
 
         CSVReader reader = new CSVReader(new FileReader(fileName), ',');
@@ -51,7 +52,6 @@ public class TestCsvReader {
     @Test
     public void testHeaderOnly() throws IOException {
         InputStream stream = new FileInputStream(fileName);
-        boolean header = true;
         Table table = CsvReader.headerOnly(fileName, types, header, ',', stream);
 
         CSVReader reader = new CSVReader(new FileReader(fileName), ',');
@@ -59,22 +59,46 @@ public class TestCsvReader {
     }
 
     @Test
-    public void testPrintColumnTypes() {
-
+    public void testPrintColumnTypes() throws IOException {
+        String columnTypes = CsvReader.printColumnTypes(fileName, header, ',');
+        String expected = "ColumnType[] columnTypes = {\n" +
+                "INTEGER,        // 0     int         \n" +
+                "BOOLEAN,        // 1     boolean     \n" +
+                "LONG_INT,       // 2     long        \n" +
+                "FLOAT,          // 3     float       \n" +
+                "SHORT_INT,      // 4     short       \n" +
+                "LOCAL_DATE,     // 5     date        \n" +
+                "LOCAL_TIME,     // 6     time        \n" +
+                "LOCAL_DATE_TIME, // 7     datetime    \n" +
+                "CATEGORY,       // 8     category    \n" +
+                "}\n";
+        assertThat(columnTypes, is(expected));
     }
 
     @Test
-    public void testDetectColumnTypes() {
-
+    public void testDetectColumnTypes() throws IOException {
+        ColumnType[] columnTypes = CsvReader.detectColumnTypes(fileName, header, ',');
+        ColumnType[] expected = types;
+        assertThat(columnTypes, is(expected));
     }
 
     @Test
     public void testNextRow() {
-
+        int input = -1;
+        int result = CsvReader.nextRow(input);
+        int expected = 0;
+        assertThat(result, is(expected));
     }
 
     @Test
     public void testDetectType() {
-
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("-34");
+        strings.add("91");
+        strings.add("0");
+        strings.add("10");
+        ColumnType result = CsvReader.detectType(null);
+        ColumnType expected = ColumnType.SKIP;
+        assertThat(result, is(expected));
     }
 }
